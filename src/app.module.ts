5c96d1bbae520fc7dec8ec40bae665a72642b911
OmarKHDR <omarsliman37@gmail.com>
2026-02-23 16:40:30 +0200

    adding text body parser

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,21 @@
 import { Module } from '@nestjs/common';
 import { SqlInterpreterModule } from './sql-interpreter/sql-interpreter.module';
-import { LexerModule } from './lexer/lexer.module';
 import { ParserModule } from './parser/parser.module';
 import { SharedModule } from './shared/shared.module';
 import { FitModule } from './fit/fit.module';
 import { StorageEngineModule } from './storage-engine/storage-engine.module';
 import { WinstonLoggerModule } from './winston-logger/winston-logger.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
     SqlInterpreterModule,
-    LexerModule,
     ParserModule,
     SharedModule,
     FitModule,
     StorageEngineModule,
     WinstonLoggerModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
+  app.use(bodyParser.text());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
