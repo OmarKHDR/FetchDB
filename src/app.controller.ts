2753a0ed9d0877c8973e6c361d5adbec9101bdf4
OmarKHDR <omarsliman37@gmail.com>
2026-02-27 22:58:46 +0200

    adding swagger api into the controller for better documentations

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,22 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SqlInterpreterService } from './sql-interpreter/sql-interpreter.service';
 import { WinstonLoggerService } from './winston-logger/winston-logger.service';
 import { StorageEngineService } from './storage-engine/storage-engine.service';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+  ApiProperty,
+} from '@nestjs/swagger';
+
+class setVersionDto {
+  @ApiProperty({ type: 'number' })
+  version: number;
+}
+class selectRowDto {
+  @ApiProperty({ type: 'number' })
+  id: number;
+}
+
 @Controller()
 export class AppController {
   constructor(
@@ -10,6 +26,29 @@ export class AppController {
     private storage: StorageEngineService,
   ) {}
 
+  @ApiConsumes('text/plain')
+  @ApiOperation({
+    description:
+      'execute a dml statment to insert, change, view, delete existing data ',
+  })
+  @ApiBody({
+    schema: { type: 'string' },
+    examples: {
+      insert: {
+        value:
+          'INSERT INTO employees (name, created_at) VALUES ("omar", "2025-11-23T10:00:00Z")',
+      },
+      select: {
+        value: 'SELECT * FROM employees;',
+      },
+      update: {
+        value: 'UPDATE TABLE employees SET name="ahmed" WHERE id=0;',
+      },
+      delete: {
+        value: 'DELETE FROM employees WHERE name="ahmed";',
+      },
+    },
+  })
   @Post('/execute/dml')
   async executeDML(@Body() body: string) {
     this.winston.logger.info(`[AppController]: recieved query: ${body}`);
@@ -23,6 +62,19 @@ export class AppController {
     }
   }
 
+  @ApiOperation({
+    description: 'executes a DDL statement to create or change db schema',
+  })
+  @ApiBody({
+    schema: { type: 'string' },
+    examples: {
+      create: {
+        value:
+          'CREATE TABLE employees (id SERIAL PRIMARY KEY, name VARCHAR(15) DEFAULT "username", created_at TIMESTAMP);',
+      },
+    },
+  })
+  @ApiConsumes('text/plain')
   @Post('/execute/ddl')
   async executeDDL(@Body() body: string) {
     try {
@@ -35,6 +87,7 @@ export class AppController {
     }
   }
 
+  @ApiOperation({ description: 'get an array of schema history' })
   @Get('/history')
   async getSchemaHistory() {
     try {
@@ -50,12 +103,16 @@ export class AppController {
     }
   }
 
+  @ApiOperation({
+    description: 'set a new schema version from the array got from /history ',
+  })
+  @ApiBody({ type: setVersionDto })
   @Post('/version')
-  async setSchemaVersion(@Body() body: { id: number }) {
+  async setSchemaVersion(@Body() body: { version: number }) {
     try {
       return {
         success: true,
-        data: await this.storage.setSchemaVersion(body.id),
+        data: await this.storage.setSchemaVersion(body.version),
       };
     } catch (err) {
       return {
@@ -65,6 +122,10 @@ export class AppController {
     }
   }
 
+  @ApiOperation({
+    description: 'get the entire history of changes of a specific row ',
+  })
+  @ApiBody({ type: selectRowDto })
   @Post('/data/history')
   async getDataHistory() {}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import bodyParser from 'body-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
+  const config = new DocumentBuilder()
+    .setTitle('FetchDB')
+    .setDescription('The FetchDB api description')
+    .setVersion('1.0')
+    .addTag('SQL Interpreter over http')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   app.use(bodyParser.text());
   await app.listen(process.env.PORT ?? 3000);
 }
