2c4ad5e9882abf3caf333f78c7529f6e1d883103
OmarKHDR <omarsliman37@gmail.com>
2026-04-22 11:48:22 +0200

    try docker

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
   app.use(bodyParser.text());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
